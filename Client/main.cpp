f40d31658542c032161bf301ae19fce2ef9d4d2f
Jora Gevorgyan <jora@ispras.ru>
2022-06-04 01:26:07 +0400

    using 5445 port to connect with server

--- Client/main.cpp
+++ Client/main.cpp
@@ -6,7 +6,7 @@ int main()
   std::string username;
   std::cout << "Enter your username: ";
   std::cin >> username;
-  Client client(uri_builder(U("http://localhost")).to_uri().to_string(), username);
+  Client client(uri_builder(U("http://localhost:5445")).to_uri().to_string(), username);
   client.start_chat(chat_duration);
   return 0;
 }
