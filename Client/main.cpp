196209e29729abe51c7f4e2c6ccd1b6a9112300c
Jora Gevorgyan <jora@ispras.ru>
2022-06-04 04:44:30 +0400

    comment out everything

--- Client/main.cpp
+++ Client/main.cpp
@@ -4,10 +4,10 @@ int main()
 {
   try {
     constexpr int chat_duration = 40;
-    std::string username;
-//    std::cout << "Enter your username: ";
-//    std::cin >> username;
-    Client client(uri_builder(U("http://localhost:5445")).to_uri().to_string(), "username");
+    std::cout << "Enter your username: ";
+    std::string username{};
+    std::cin >> username;
+    Client client(uri_builder(U("http://localhost:5445")).to_uri().to_string(), username);
     client.start_chat(chat_duration);
   }
   catch (const web::http::http_exception& exc) {
